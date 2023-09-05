a7a433485baff8c3d144b46db82907d6d3fe4149
Bin <zzbsdh000@outlook.com>
2023-09-05 21:16:46 -0230

    Remove redundant code

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,3 @@ function App() {
 }
 
 export default App;
-
-/* <div className="products">{productElements}</div> */
-
-// Object { id: 1, title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", price: 109.95, … }
-// category: "men's clothing"
-// description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
-// id: 1
-// image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-// price: 109.95
-// rating: Object { rate: 3.9, count: 120 }
-// title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
